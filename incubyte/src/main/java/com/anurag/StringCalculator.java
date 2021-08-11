30301226b1ff1d24cc26777ceecf62f926806557
aup-axelor <a.updhyay@axelor.com>
2021-08-11 16:41:07 +0530

    Refactored to add two numbers

--- incubyte/src/main/java/com/anurag/StringCalculator.java
+++ incubyte/src/main/java/com/anurag/StringCalculator.java
@@ -9,6 +9,7 @@ public class StringCalculator
     public int add(String number) {
         if(number.equals("")) return 0;
         if(number.length()==1) return Integer.valueOf(number);
-        return 0;
+        String[] numbers=number.split(",");
+        return Integer.valueOf(numbers[0])+Integer.valueOf(numbers[1]);
     }
 }
