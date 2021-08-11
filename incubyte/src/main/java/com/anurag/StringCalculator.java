c6cedd00b4b370633114620876eba30446b2bd20
aup-axelor <a.updhyay@axelor.com>
2021-08-11 14:01:56 +0530

    Initial Commit

---
+++ incubyte/src/main/java/com/anurag/StringCalculator.java
@@ -0,0 +1,13 @@
+package com.anurag;
+
+/**
+ * Hello world!
+ *
+ */
+public class StringCalculator 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ incubyte/src/test/java/com/anurag/StringCalculatorTest.java
@@ -0,0 +1,20 @@
+package com.anurag;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class StringCalculatorTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
